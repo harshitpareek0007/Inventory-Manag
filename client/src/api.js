54e920773a60d23b33ff367818bb62e2b352474a
harshitpareek0007 <harshitpareek0007@gmail.com>
2026-05-28 15:38:40 +0530

    vs code somit

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // change to https://inventory-management-yifk.onrender.com for production
+  baseURL: 'https://inventory-management-yifk.onrender.com', // change to https://inventory-management-yifk.onrender.com for production
 });
 
 export default api;
